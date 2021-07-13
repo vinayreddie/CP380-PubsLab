8ddab82c58db399ca3cf7cccbf2e4d11c04bd86b
bildzeitung <bildzeitung@gmail.com>
2021-07-12 23:15:59 -0400

    Add project files.

---
+++ CP380-PubsLab/Models/PubsDbContext.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.IO;
+
+
+namespace CP380_PubsLab.Models
+{
+    public class PubsDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var dbpath = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\\..\\..\\pubs.mdf"));
+            optionsBuilder.UseSqlServer($"Server=(localdb)\\mssqllocaldb;Integrated Security=true;AttachDbFilename={dbpath}");
+        }
+
+        // TODO: Add DbSets
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // TODO
+        }
+    }
+
+
+    public class Titles
+    {
+        // TODO
+    }
+
+
+    public class Stores
+    {
+        // TODO
+    }
+
+
+    public class Sales
+    {
+        // TODO
+    }
+
+    public class Employee
+    {
+        // TODO
+    }
+
+    public class Jobs
+    {
+        // TODO
+    }
+}
---
+++ CP380-PubsLab/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CP380_PubsLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var dbcontext = new Models.PubsDbContext())
+            {
+                if (dbcontext.Database.CanConnect())
+                {
+                    Console.WriteLine("Yes, I can connect");
+                }
+
+                // 1:Many practice
+                //
+                // TODO: - Loop through each employee
+                //       - For each employee, list their job description (job_desc, in the jobs table)
+
+                // TODO: - Loop through all of the jobs
+                //       - For each job, list the employees (first name, last name) that have that job
+
+
+                // Many:many practice
+                //
+                // TODO: - Loop through each Store
+                //       - For each store, list all the titles sold at that store
+                //
+                // e.g.
+                //  Bookbeat -> The Gourmet Microwave, The Busy Executive's Database Guide, Cooking with Computers: Surreptitious Balance Sheets, But Is It User Friendly?
+                
+                // TODO: - Loop through each Title
+                //       - For each title, list all the stores it was sold at
+                //
+                // e.g.
+                //  The Gourmet Microwave -> Doc-U-Mat: Quality Laundry and Books, Bookbeat
+            }
+        }
+    }
+}
